d8884cbc83c0a33bd6d15c0d5d4b416fdf3f0647
profeale <aleprieto@mi.unc.edu.ar>
2022-11-14 20:56:17 -0300

    js para conversor

---
+++ frontend/js/cotizacion.js
@@ -0,0 +1,11 @@
+function Convertir() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEnDolarNumerico = parseFloat(valor);
+  
+    var valorEnPeso = valorEnDolarNumerico * 130;
+    console.log(valorEnPeso);
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "El resultado en pesos del dolar venta es $" + valorEnPeso;
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }
