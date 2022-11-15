9fec1f1a907b2232a819dddf90b502ecd3985b3a
profeale <aleprieto@mi.unc.edu.ar>
2022-11-14 21:50:46 -0300

    correcciones codigo

--- frontend/js/cotizacion.js
+++ frontend/js/cotizacion.js
@@ -3,7 +3,7 @@ function Convertir() {
     var valor = valorElemento.value;
     var valorEnDolarNumerico = parseFloat(valor);
   
-    var valorEnPeso = valorEnDolarNumerico * 130;
+    var valorEnPeso = valorEnDolarNumerico * 157.25;
     console.log(valorEnPeso);
     var elementoValorConvertido = document.getElementById("valorConvertido");
     var valorConvertido = "El resultado en pesos del dolar venta es $" + valorEnPeso;
